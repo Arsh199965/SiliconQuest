a79ff6ef4f13f734904a4f9ccdd5c5c14082193a
Arsh Ahmad <arsh199965@gmail.com>
2025-10-15 02:05:14 +0530

    Finalizing changes from my side -arsh

--- client/src/app/metadata.ts
+++ client/src/app/metadata.ts
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "SiliconQuest - Animeverse Hunt",
+  title: "SiliconQuest: Animeverse",
   description: "Start your character hunting adventure in Animeverse",
   manifest: "/manifest.json",
 };
